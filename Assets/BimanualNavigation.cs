d31b7eda9806d142d40a98e4bbc60b5cc326bf39
adamdrogemuller <adamdrogemuller@users.noreply.github.com>
2019-04-18 15:02:12 +0930

    Oculus Support
    Oculus Support, Bimanual Navigation and Interaction.

---
+++ Assets/BimanualNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BimanualNavigation : MonoBehaviour {
+
+    public TrackedObject t1;
+    public TrackedObject t2;
+    public Transform vrPerson;
+    public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(t1.pressed > 0.5f && t2.pressed > 0.5f)
+        {
+            vrPerson.transform.position += ((t2.transform.position - t1.transform.position))*speed;
+        }
+	}
+}
---
+++ Assets/TrackedObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackedObject : MonoBehaviour {
+
+    public enum Hand {Left,Right};
+    public Hand trackedController;
+    public float pressed = 0;
+    public float secondaryPressed = 0;
+    public Transform node;
+    private Transform prevParent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (trackedController == Hand.Right)
+        {
+
+            pressed = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
+            secondaryPressed = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch);
+        }
+        if (trackedController == Hand.Left)
+        {
+
+            pressed = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+            secondaryPressed = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        }
+        if(secondaryPressed < 0.1f && node != null)
+        {
+            node.transform.parent = null;
+            node = null;
+        }
+        if(secondaryPressed > 0.5f && node != null)
+        {
+            prevParent = node.transform.parent;
+            node.transform.position = transform.position;
+            node.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.GetComponent<VirtualNode>() && node == null)
+        {
+            node = other.transform;
+            prevParent = node.transform.parent;
+        }
+        print(other.transform);
+    }
+}
